47575777ca2e2473ab5176149d888e351e3e0f75
yDonaldMcDonald <yeboahdonaldmcdonald@gmail.com>
2023-06-16 08:50:58 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - A program that prints  printf function
+*Return: 0 (Success)
+*/
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
