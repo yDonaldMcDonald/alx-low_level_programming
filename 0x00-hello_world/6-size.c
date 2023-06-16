d94eac1c7604ad004cd0ffd229c8e1856f4bf140
yDonaldMcDonald <yeboahdonaldmcdonald@gmail.com>
2023-06-16 09:33:18 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,11 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("size of a char:%lu byte(s)\n", (unsigned long)siszeof(a));
-printf("size of an int:%lu byte(s)\n", (unsigned long)siszeof(b));
-printf("size of a long int:%lu bytes(s)\n", (unsigned long)siszeof(c));
-printf("size of a long long int:%lu bytes(s)\n", (unsigned long)siszeof(d));
-printf("size of a float:%lu bytes(s)\n", (unsigned long)siszeof(f));
+
+printf("size of a char: %lu byte(s)\n", (unsigned long)siszeof(a));
+printf("size of an int: %lu byte(s)\n", (unsigned long)siszeof(b));
+printf("size of a long int: %lu bytes(s)\n", (unsigned long)siszeof(c));
+printf("size of a long long int: %lu bytes(s)\n", (unsigned long)siszeof(d));
+printf("size of a float: %lu bytes(s)\n", (unsigned long)siszeof(f));
 return (0);
 }
